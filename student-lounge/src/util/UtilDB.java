a150561532b93a8be473e6decdad263383d3a4ee
alhar <alhar@DESKTOP-BTR0FNO>
2021-11-20 00:35:12 -0600

    Adding main components

---
+++ student-lounge/src/util/UtilDB.java
@@ -0,0 +1,26 @@
+/**
+ */
+package util;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ * @since JavaSE-1.8
+ */
+public class UtilDB {
+
+}
