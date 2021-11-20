8452b783dbc127b2d0b87aca75acc856093dc4bb
alhar <alhar@DESKTOP-BTR0FNO>
2021-11-19 23:13:18 -0600

    linking SignUp with java authentication

--- student-lounge/src/authentication/Login.java
+++ student-lounge/src/authentication/Login.java
@@ -31,7 +31,7 @@ public class Login extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("index.html");
 	}
 
 	/**
---
+++ student-lounge/src/authentication/Signup.java
@@ -0,0 +1,52 @@
+package authentication;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Signup
+ */
+@WebServlet("/Signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signup() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("user");
+		String password = request.getParameter("pass");
+		String cpassword = request.getParameter("cpass");
+		String email = request.getParameter("email");
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.append(username + " " + password + " " + cpassword + " " + email + " " + " " + fname + " " + lname);
+	}
+
+}
