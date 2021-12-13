607d28bd29cb17238d078d1487226958656ca82a
alhar <alhar@DESKTOP-BTR0FNO>
2021-12-13 17:14:54 -0600

    Reset password implemented

--- student-lounge/src/authentication/Reset.java
+++ student-lounge/src/authentication/Reset.java
@@ -8,7 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mailjet.client.resource.Emailv31;
+
+import datamodel.User;
+import util.Hash;
+import util.UtilDB;
+
 import java.util.Properties;
+import java.util.Random;
+
 import javax.mail.AuthenticationFailedException;
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
@@ -106,12 +124,106 @@ public class Reset extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		//processRequest(request, response);
+
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		//processRequest(request, response);
+		
+		String email = request.getParameter("email");
+		
+		try
+		{
+			User user = UtilDB.getUsers(email, "email").get(0);
+			
+			if (user != null)
+			{
+				String capitalCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+				String specialCharacters = "!@#$";
+				String numbers = "1234567890";
+				String combinedChars = capitalCaseLetters + lowerCaseLetters + specialCharacters + numbers;
+				Random random = new Random();
+				int length = 20;
+				char[] password = new char[length];
+
+				password[0] = lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
+				password[1] = capitalCaseLetters.charAt(random.nextInt(capitalCaseLetters.length()));
+				password[2] = specialCharacters.charAt(random.nextInt(specialCharacters.length()));
+				password[3] = numbers.charAt(random.nextInt(numbers.length()));
+
+				for (int i = 4; i < length; i++) {
+					password[i] = combinedChars.charAt(random.nextInt(combinedChars.length()));
+				}
+				
+				UtilDB.editUser(email, Hash.hash(password.toString()), "password");
+
+				MailjetClient client;
+				MailjetRequest requestt;
+				MailjetResponse responset = null;
+				client = new MailjetClient("e2fc74790fb07fbece050767a3fae8eb", "185878afcdd3dfbc4050319fd5ddeaca",
+						new ClientOptions("v3.1"));
+				requestt = new MailjetRequest(Emailv31.resource).property(Emailv31.MESSAGES,
+						new JSONArray().put(new JSONObject()
+								.put(Emailv31.Message.FROM,
+										new JSONObject().put("Email", "qsx560560@gmail.com").put("Name", "Student-Lounge-Team"))
+								.put(Emailv31.Message.TO,
+										new JSONArray()
+												.put(new JSONObject().put("Email", email).put("Name", user.getFirstName() + " " + user.getLastName())))
+								.put(Emailv31.Message.SUBJECT, "Student-Lounge Password reset.")
+								.put(Emailv31.Message.TEXTPART, "Password rest email")
+								.put(Emailv31.Message.HTMLPART,
+										String.format("<h1>Welcome %s</h1> <br> <h3>Here is your temporary password: %s</h3>", user.getFirstName() + " " + user.getLastName(), password.toString()))
+								.put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
+				try {
+					responset = client.post(requestt);
+				} catch (MailjetException | MailjetSocketTimeoutException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(responset.getStatus());
+				System.out.println(responset.getData());
+				
+				
+				if (responset.getStatus() == 200)
+				{
+					String wronginput = "A temporary password was sent to your email, check your spam if you did not see anything.";
+					request.setAttribute("wronginput", wronginput);
+					RequestDispatcher rd = request.getRequestDispatcher("reset.jsp");
+					rd.forward(request, response);
+				}
+				else
+				{
+					String wronginput = "<div class=\"alert alert-warning\">\r\n"
+							+ "  <strong>Warning!</strong> The email was not sent an error occured!.\r\n" + "</div>";
+					
+					request.setAttribute("wronginput", wronginput);
+					RequestDispatcher rd = request.getRequestDispatcher("reset.jsp");
+					rd.forward(request, response);
+				}
+			}
+			else
+			{
+				String wronginput = "<div class=\"alert alert-warning\">\r\n"
+						+ "  <strong>Warning!</strong> The email dose not exist!.\r\n" + "</div>";
+				
+				request.setAttribute("wronginput", wronginput);
+				RequestDispatcher rd = request.getRequestDispatcher("reset.jsp");
+				rd.forward(request, response);
+			}
+		}
+		catch (IndexOutOfBoundsException ie)
+		{
+			String wronginput = "<div class=\"alert alert-warning\">\r\n"
+					+ "  <strong>Warning!</strong> The email dose not exist!.\r\n" + "</div>";
+			
+			request.setAttribute("wronginput", wronginput);
+			RequestDispatcher rd = request.getRequestDispatcher("reset.jsp");
+			rd.forward(request, response);
+		}
+		
 	}
 
 }
